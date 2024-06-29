dd4d2f3ee8a6fc4f41315fd57682b5caf11cec50
SERGEI MLK <sergei.mlk@hotmail.com>
2024-06-29 18:39:25 +0200

    lastbeforePROJECTS

--- src/components/Pre.js
+++ src/components/Pre.js
@@ -1,6 +1,10 @@
+// Pre.js
 import React from "react";
+
 function Pre(props) {
-  return <div id={props.load ? "preloader" : "preloader-none"}></div>;
+  return (
+    <div id={props.load ? "preloader" : "preloader-none"} className={props.load ? "fast-spin" : ""}></div>
+  );
 }
 
 export default Pre;
